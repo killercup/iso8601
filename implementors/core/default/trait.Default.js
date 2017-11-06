c318af2e8fb7a560e6a3bdb07babba17a66c47a0
Travis CI User <travis@example.org>
2017-11-06 09:55:30 +0000

    Documentation for badboy/iso8601@389f858

---
+++ implementors/core/default/trait.Default.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["iso8601"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"iso8601/struct.Time.html\" title=\"struct iso8601::Time\">Time</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"iso8601/struct.DateTime.html\" title=\"struct iso8601::DateTime\">DateTime</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"enum\" href=\"iso8601/enum.Date.html\" title=\"enum iso8601::Date\">Date</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
